ef68c2516181ce445073e8b94ee078fc60185bbe
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-23 18:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+			let select=document.getElementById("colorSelect");
+            let button=document.getElementsByTagName("input")[0];
+            button.addEventListener('click',()=>{
+                let rem=select.value;
+                for(let i=0;i<select.children.length;i++){
+                    if(select.children[i].innerText == rem){
+                        select.removeChild(select.children[i]);
+                    }
+                }
+            })
